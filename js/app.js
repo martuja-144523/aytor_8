8aebd60daaf1bd082302a2092033727b9b7c7018
martuja-144523 <116412317+martuja-144523@users.noreply.github.com>
2023-01-04 08:31:49 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,67 @@
+$(function() {
+    $(window).load(function(){
+        $('#loder').fadeOut('slow')
+    });
+
+
+    $('.slide').slick({
+        arrows: false ,
+        autoplay: true,
+        autoplaySpeed: 5000 ,
+        dots:true,
+        dotsClass: 'ban_dots',
+    });
+
+    $('.recent_product_slid').slick({
+        slidesToShow: 4,
+        prevArrow :'<i class="fa-solid fa-chevron-left recent_slider_icon slide_icon_left"></i>',
+        nextArrow :'<i class="fa-solid fa-chevron-right recent_slider_icon slide_icon_right"></i>'
+    });
+    
+    $('.deals_slide').slick({
+        slidesToShow: 2 ,
+        arrows: false ,
+        dots:true,
+        dotsClass: 'ban_dots',
+    })
+
+    
+
+    $('#count_down').countdown('2023/12/31' , function(event) {
+        var $this = $(this).html(event.strftime(''
+        + '<div class="count_down_child"><span class="up">%D</span><span class="down">Days</span></div>'
+        + '<span class="outer_span">:</span> '
+        + '<div class="count_down_child"><span class="up">%H</span><span class="down">Hours</span></div> '
+        + '<span class="outer_span">:</span> '
+        + '<div class="count_down_child"><span class="up">%M</span><span class="down">Miniutes</span></div>'
+        + '<span class="outer_span">:</span> '
+        + '<div class="count_down_child"><span class="up">%S</span><span class="down">Seconds</span></div>'
+    ))
+    });
+
+    $('.news_slide').slick({
+        slidesToShow: 4 ,
+        arrows: false ,
+        dots:true,
+        dotsClass: 'ban_dots',
+    });
+
+
+    $(window).on('scroll' , function () {
+        var scrollSize = $(window).scrollTop()
+
+        if (scrollSize > 100 ) {
+            $('#mynav').addClass('.fixd')
+        }else{
+            $('#mynav').removeClass('.fixd')
+            
+        }
+    });
+})
+
+new VenoBox({
+    selector: ".my_video"
+});
+
+
+
